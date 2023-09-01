ec2e32b2fbe8f5fa275a02768ff30b79ffef99ee
fromant <fromant65@gmail.com>
2023-09-01 17:51:35 -0300

    Finished game logic

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -1,9 +1,11 @@
+const fillerString = " ";
+
 export class Cell {
   state: string | Board;
   isSelected: boolean = false;
   constructor(board: Board | null) {
     if (board) this.state = board;
-    else this.state = "_";
+    else this.state = fillerString;
   }
   setState(player: "O" | "X" | Board) {
     this.state = player;
@@ -17,12 +19,78 @@ export class Cell {
   getPlayer(): string {
     return typeof this.state === "string" ? this.state : "";
   }
+  getBoard(): Board | null {
+    return this.state instanceof Board ? this.state : null;
+  }
 }
 
 export class Board {
+  winner: string | null = null;
+  finished = false;
   cells: Cell[][] = [];
   turn: number = 0;
   constructor(cells?: Cell[][]) {
     if (cells) this.cells = cells;
   }
+  setWinner(winner: string) {
+    this.winner = winner;
+  }
+
+  checkEnd() {
+    //checks if a board is finished and determines the winner
+    let cells = this.cells;
+    if (typeof cells[0][0].getState() === "string") {
+      this.#checkFilesAndColumns(cells);
+      this.#checkDiagonals(cells);
+      this.#checkIsBoardFull(cells);
+    }
+  }
+
+  #checkFilesAndColumns(cells: Cell[][]) {
+    for (let i = 0; i < 3; i++) {
+      if (
+        cells[i][0].getState() === cells[i][1].getState() &&
+        cells[i][0].getState() === cells[i][2].getState() &&
+        cells[i][0].getState() != fillerString
+      ) {
+        this.finished = true;
+        this.winner = cells[i][0].getPlayer();
+      }
+      if (
+        cells[0][i].getState() === cells[1][i].getState() &&
+        cells[0][i].getState() === cells[2][i].getState() &&
+        cells[0][i].getState() != fillerString
+      ) {
+        this.finished = true;
+        this.winner = cells[0][i].getPlayer();
+      }
+    }
+  }
+  #checkDiagonals(cells: Cell[][]) {
+    if (
+      (cells[0][0].getState() === cells[1][1].getState() &&
+        cells[0][0].getState() === cells[2][2].getState() &&
+        cells[0][0].getState() != fillerString) ||
+      (cells[2][0].getState() === cells[1][1].getState() &&
+        cells[2][0].getState() === cells[0][2].getState() &&
+        cells[2][0].getState() != fillerString)
+    ) {
+      this.finished = true;
+      this.winner = cells[1][1].getPlayer();
+    }
+  }
+  #checkIsBoardFull(cells: Cell[][]) {
+    let occupiedCells = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let cellState = cells[i][j].getPlayer();
+        if (cellState === "X" || cellState === "O") occupiedCells++;
+      }
+    }
+    if (occupiedCells === 9) this.finished = true;
+  }
+}
+
+export class Game {
+  turn: number = 0;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,14 +20,68 @@
     return new Board(cells);
   }
   let board = createBoard("super");
-  //console.log(board);
+  let lastMove: number[] = [];
 
   function handleMove(i: number, j: number, k: number, l: number) {
+    if (!checkValidMove(i, j, k, l)) return;
     board.turn % 2 === 0
       ? board.cells[i][j].getCells()[k][l].setState("X")
       : board.cells[i][j].getCells()[k][l].setState("O");
     board.turn++;
+
+    board.cells[i][j].getBoard()?.checkEnd();
+    if (board.cells[i][j].getBoard()?.winner) {
+      let winner = board.cells[i][j].getBoard()?.winner;
+      if (winner === "O" || winner === "X") board.cells[i][j].setState(winner);
+    }
+    checkGameEnding();
+    if (board.winner === "X" || board.winner === "O") {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (!board.cells[i][j].getPlayer())
+            board.cells[i][j].setState(board.winner);
+        }
+      }
+    }
+    lastMove = [i, j, k, l];
     board = board; //Triggers update on state
+  }
+
+  function checkValidMove(i: number, j: number, k: number, l: number) {
+    let cell = board.cells[i][j].getCells()[k][l].getPlayer();
+    if (cell === "X" || cell === "O") return false;
+    //If last move's cell isn't current move's board,
+    //and last move's correspondant board isn't finished or has no winner, don't move
+    let lastk = lastMove[2] || 0;
+    let lastl = lastMove[3] || 0;
+    let currentBoard = board.cells[lastk][lastl];
+    if (
+      lastMove.length &&
+      (lastk != i || lastl != j) &&
+      !currentBoard.getPlayer() &&
+      !currentBoard.getBoard()?.finished
+    )
+      return false;
+    //If the selected board is already finished, don't move
+    if (board.cells[i][j].getBoard()?.finished) return false;
+    return true;
+  }
+
+  function checkGameEnding() {
+    board.checkEnd();
+    if (board.finished && !board.winner) {
+      let x = 0;
+      let o = 0;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board.cells[i][j].getBoard()?.winner === "O") o++;
+          if (board.cells[i][j].getBoard()?.winner === "X") x++;
+        }
+      }
+      o > x && board.setWinner("O");
+      x > o && board.setWinner("X");
+    }
+    board = board;
   }
 </script>
 
@@ -35,21 +89,49 @@
   <div class="super-board">
     {#each board.cells as row, i}
       {#each row as cell, j}
-        <div class={`board-container`}>
-          {#each cell.getCells() as iRow, k}
-            {#each iRow as iCell, l}
-              <button class="cell" on:click={() => handleMove(i, j, k, l)}>
-                {iCell.getState()}
-              </button>
+        <div
+          class={`${
+            cell.getState() === "O" || cell.getState() === "X"
+              ? "finished-board"
+              : "board-container"
+          } 
+          ${lastMove[2] === i && lastMove[3] === j ? "active" : ""}
+          `}
+        >
+          {#if cell.getState() === "X" || cell.getState() === "O"}
+            <div>{cell.getState()}</div>
+          {:else}
+            {#each cell.getCells() as iRow, k}
+              {#each iRow as iCell, l}
+                <button class="cell" on:click={() => handleMove(i, j, k, l)}>
+                  {iCell.getState()}
+                </button>
+              {/each}
             {/each}
-          {/each}
+          {/if}
         </div>
       {/each}
     {/each}
   </div>
+  <p>
+    {board.finished && board.winner
+      ? `Winner is ${board.winner}`
+      : board.finished
+      ? "DRAW"
+      : ""}
+  </p>
 </main>
 
 <style>
+  * {
+    color: black;
+    font-weight: 1000;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  main {
+    display: flex;
+  }
   .super-board {
     display: grid;
     grid-template-columns: repeat(3, 200px);
@@ -64,9 +146,18 @@
     grid-template-columns: repeat(3, 50px);
     gap: 0.25rem;
   }
+  .finished-board {
+    display: flex;
+    align-items: center;
+    justify-items: center;
+  }
+
   .cell {
     border: 1px solid black;
     padding: 0.5rem;
     text-align: center;
+  }
+  .active {
+    background-color: gray;
   }
 </style>
